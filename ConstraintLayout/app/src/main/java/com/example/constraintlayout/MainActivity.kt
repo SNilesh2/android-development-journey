d5ba76ecb8399f3973fb2fbe81d5a245466ee872
nilesh <snileshofficial@gmail.com>
2026-05-28 23:49:03 +0530

    Added constraint Layout jetpack compose

---
+++ ConstraintLayout/app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.constraintlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import com.example.constraintlayout.ui.theme.ConstraintLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val contrains = ConstraintSet{
+                val greenBox = createRefFor("greenBox")
+                val blueBox = createRefFor("blueBox")
+
+                constrain(greenBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+                constrain(blueBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                createHorizontalChain(greenBox,blueBox, chainStyle = ChainStyle.Spread)
+            }
+
+            ConstraintLayout(
+                contrains,
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .layoutId("greenBox")
+                )
+
+                Box(
+                    modifier = Modifier
+                        .background(Color.Blue)
+                        .layoutId("blueBox")
+                )
+            }
+        }
+    }
+}
+
